db7440d92c2ecbd7d2ec2d804bc119878ea0c9df
Diego Pagini <dq@qbkconsulting.com>
2021-02-16 07:15:16 -0300

    mensaje personalizado para offline

---
+++ js/app.js
@@ -0,0 +1,6 @@
+/** @format */
+
+//Instalar el SW
+if (navigator.serviceWorker) {
+	navigator.serviceWorker.register('/sw.js');
+}
--- sw.js
+++ sw.js
@@ -1,3 +1,12 @@
+/** @format */
 
-
-
+self.addEventListener('fetch', (event) => {
+	const offlineResp = new Response(`
+    Bienvenido a mi Pagina web,
+    Disculpa, pero para usarla, necesitas internet.
+  `);
+	const resp = fetch(event.request).catch(() => {
+		return offlineResp;
+	});
+	event.respondWith(resp);
+});
