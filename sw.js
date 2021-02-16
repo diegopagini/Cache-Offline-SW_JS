8c9fb20a91e835b655b82571d9915ac67e0a9048
Diego Pagini <dq@qbkconsulting.com>
2021-02-16 15:21:11 -0300

    controlar el crecimiento del cache dynamic

--- sw.js
+++ sw.js
@@ -1,22 +1,39 @@
 /** @format */
 
-const CACHE_NAME = 'cache-1';
+// const CACHE_NAME = 'cache-1';
+const CACHE_STATIC_NAME = 'static-v2';
+const CACHE_DYNAMIC_NAME = 'dynamic-v1';
+const CACHE_INMUTABLE_NAME = 'inmutable-v1';
+
+function limpiarCache(cacheName, numeroItems) {
+	caches.open(cacheName).then((cache) => {
+		return cache.keys().then((keys) => {
+			if (keys.length > numeroItems) {
+				cache.delete(keys[0]).then(limpiarCache(cacheName, numeroItems));
+			}
+		});
+	});
+}
 
 //App shell. (todo lo que requiere la app para funcionar)
 self.addEventListener('install', (event) => {
-	const cacheProm = caches.open(CACHE_NAME).then((cache) => {
+	const cacheProm = caches.open(CACHE_STATIC_NAME).then((cache) => {
 		//Grabando todo en el cache.
 		return cache.addAll([
 			'/',
 			'/index.html',
 			'/css/style.css',
 			'/img/main.jpg',
-			'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css',
 			'/js/app.js',
 		]);
 	});
+	const cacheInmutable = caches.open(CACHE_INMUTABLE_NAME).then((cache) => {
+		return cache.addAll([
+			'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css',
+		]);
+	});
 	//Esperar a que todo lo anterior este cargado.
-	event.waitUntil(cacheProm);
+	event.waitUntil(Promise.all([cacheProm, cacheInmutable]));
 });
 
 self.addEventListener('fetch', (event) => {
@@ -31,8 +48,9 @@ self.addEventListener('fetch', (event) => {
 		console.log('No existe', event.request.url);
 
 		return fetch(event.request).then((newResp) => {
-			caches.open(CACHE_NAME).then((cache) => {
+			caches.open(CACHE_DYNAMIC_NAME).then((cache) => {
 				cache.put(event.request, newResp);
+				limpiarCache(CACHE_DYNAMIC_NAME, 50);
 			});
 			return newResp.clone();
 		});
