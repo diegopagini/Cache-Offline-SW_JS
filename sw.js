06f58327ef3d223b596f4070349608a88f953b3a
Diego Pagini <dq@qbkconsulting.com>
2021-02-16 08:17:09 -0300

    estragetia cache with network fallback

--- sw.js
+++ sw.js
@@ -1,8 +1,10 @@
 /** @format */
 
+const CACHE_NAME = 'cache-1';
+
 //App shell. (todo lo que requiere la app para funcionar)
 self.addEventListener('install', (event) => {
-	const cacheProm = caches.open('cache-1').then((cache) => {
+	const cacheProm = caches.open(CACHE_NAME).then((cache) => {
 		//Grabando todo en el cache.
 		return cache.addAll([
 			'/',
@@ -19,5 +21,22 @@ self.addEventListener('install', (event) => {
 
 self.addEventListener('fetch', (event) => {
 	//1- CACHE ONLY: toda la app desde el cache.
-	event.respondWith(caches.match(event.request));
+	// event.respondWith(caches.match(event.request));
+
+	//2- CACHE WITH NETWORK FALLBACK
+	const respuesta = caches.match(event.request).then((resp) => {
+		if (resp) return resp;
+
+		//Si no existe el archivo lo busco en la web.
+		console.log('No existe', event.request.url);
+
+		return fetch(event.request).then((newResp) => {
+			caches.open(CACHE_NAME).then((cache) => {
+				cache.put(event.request, newResp);
+			});
+			return newResp.clone();
+		});
+	});
+
+	event.respondWith(respuesta);
 });
