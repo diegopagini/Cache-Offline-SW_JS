c2ecdff30fb0a017a77906d93532b94d38da4449
Diego Pagini <dq@qbkconsulting.com>
2021-02-16 07:58:19 -0300

    guardar el app shell cuando se instala el sw

--- sw.js
+++ sw.js
@@ -1,37 +1,17 @@
 /** @format */
 
-self.addEventListener('fetch', (event) => {
-	// const offlineResp = new Response(`
-	//   Bienvenido a mi Pagina web,
-	//   Disculpa, pero para usarla, necesitas internet.
-	// `);
-
-	// 	const offlineResp = new Response(
-	// 		`
-	//   <!DOCTYPE html>
-	//   <html lang="en">
-	// 	<head>
-	// 		<meta charset="UTF-8" />
-	// 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-	// 		<meta http-equiv="X-UA-Compatible" content="ie=edge" />
-	// 		<title>Mi PWA</title>
-	// 	</head>
-	// 	<body class="container p-3">
-	//   <h1>Offline Mode</h1>
-	//   </body>
-	//   </html>
-	// `,
-	// 		{
-	// 			headers: {
-	// 				'Content-Type': 'text/html',
-	// 			},
-	// 		}
-	// 	);
-
-	const offlineResp = fetch('pages/offline.html');
-
-	const resp = fetch(event.request).catch(() => {
-		return offlineResp;
+//App shell. (todo lo que requiere la app para funcionar)
+self.addEventListener('install', (event) => {
+	const cacheProm = caches.open('cache-1').then((cache) => {
+		//Grabando todo en el cache.
+		return cache.addAll([
+			'/index.html',
+			'/css/style.css',
+			'/img/main.jpg',
+			'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css',
+			'/js/app.js',
+		]);
 	});
-	event.respondWith(resp);
+	//Esperar a que todo lo anterior este cargado.
+	event.waitUntil(cacheProm);
 });
